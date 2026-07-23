e63b360127c54c97311065a500bdd9df8f361e37
Luis Palomino Morón <lpalominom4c@gmail.com>
2026-07-23 08:50:42 +0200

    Minor fixes in ex0

--- ex0/lambda_spells.py
+++ ex0/lambda_spells.py
@@ -2,68 +2,72 @@ def artifact_sorter(artifacts: list[dict]) -> list[dict]:
     return sorted(artifacts, key=lambda artifact: artifact["power"],
                   reverse=True)
 
+
 def power_filter(mages: list[dict], min_power: int) -> list[dict]:
     return list(filter(lambda m: m["power"] >= min_power, mages))
 
+
 def spell_transformer(spells: list[str]) -> list[str]:
     return list(map(lambda s: f"* {s} *", spells))
+
 
 def mage_stats(mages: list[dict]) -> dict:
     return (lambda p: {"max_power": max(p), "min_power": min(p),
                        "avg_power": sum(p) / len(p)})([m["power"]
                                                        for m in mages])
 
+
 def main() -> None:
-    artifacts: list[dict] = (
+    artifacts: list[dict] = [
         {
-            "name":"Return Scepter",
-            "power":10,
-            "type":"Staff" 
+            "name": "Return Scepter",
+            "power": 10,
+            "type": "Staff"
         },
         {
-            "name":"Narsil",
-            "power":90,
-            "type":"Sword" 
+            "name": "Narsil",
+            "power": 90,
+            "type": "Sword"
         },
         {
-            "name":"Gold Clock",
-            "power":50,
-            "type":"Clock" 
+            "name": "Gold Clock",
+            "power": 50,
+            "type": "Clock"
         },
         {
-            "name":"100 acre Book",
-            "power":30,
-            "type":"Book" 
+            "name": "100 acre Book",
+            "power": 30,
+            "type": "Book"
         }
-    )
-    mages: list[dict] = (
+    ]
+    mages: list[dict] = [
         {
-            "name":"Rasmodius",
-            "power":70,
-            "element":"Psychic"
+            "name": "Rasmodius",
+            "power": 70,
+            "element": "Psychic"
         },
         {
-            "name":"Gandalf",
-            "power":95,
-            "element":"Light"
+            "name": "Gandalf",
+            "power": 95,
+            "element": "Light"
         },
         {
-            "name":"Merlin",
-            "power":55,
-            "element":"Water"
+            "name": "Merlin",
+            "power": 55,
+            "element": "Water"
         },
         {
-            "name":"Krobus",
-            "power":42,
-            "element":"Darkness"
+            "name": "Krobus",
+            "power": 42,
+            "element": "Darkness"
         }
-    )
-    spells: list[str] = (
+    ]
+    spells: list[str] = [
         "Word of Power",
         "Teleport",
         "Telekinesis",
         "Shadow Step"
-    )
+    ]
     print("Testing artifact sorter...")
     print(artifact_sorter(artifacts))
     print("\nTesting power filter...")
